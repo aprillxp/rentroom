d761a90f5485e06a08e62e96a5d54e89a101b861
unknown <gavrilariendra00@gmail.com>
2025-10-24 19:06:06 +0700

    feat(country): add pagination feature for country side

--- handlers/public/country/list.go
+++ handlers/public/country/list.go
@@ -4,16 +4,35 @@ import (
 	"net/http"
 	"rentroom/models"
 	"rentroom/utils"
+	"strconv"
 
 	"gorm.io/gorm"
 )
 
 func CountryList(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// ADD (Query params for pagination)
+		pageStr := r.URL.Query().Get("page")
+		limitStr := r.URL.Query().Get("limit")
+
+		// PARSING PAGE & LIMIT
+		page, err := strconv.Atoi(pageStr)
+		if err != nil || page < 1 {
+			page = 1
+		}
+		limit, err := strconv.Atoi(limitStr)
+		if err != nil || limit < 1 {
+			limit = 10
+		}
+		offset := (page - 1) * limit
+
 		// QUERY
 		var countries []models.Country
-		err := db.
-			Find(&countries).Error
+
+		var total int64
+		db.Model(&models.Country{}).Count(&total)
+
+		err = db.Offset(offset).Limit(limit).Find(&countries).Error
 		if err != nil {
 			utils.JSONError(w, err.Error(), http.StatusInternalServerError)
 			return
