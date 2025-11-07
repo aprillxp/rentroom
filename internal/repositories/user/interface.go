4e5bf4704f0f38c3199f8270706517a791542195
your_username <you@example.com>
2025-11-07 14:31:47 +0700

    refactor(user): reorder repository interface and add error gor getByID

--- internal/repositories/user/interface.go
+++ internal/repositories/user/interface.go
@@ -3,13 +3,14 @@ package repository
 import "rentroom/internal/models"
 
 type UserRepository interface {
-	FindByIdentifier(identifier string) (*models.User, error)
-	Create(user *models.User) error
-	Update(user *models.User) error
 	FindByID(id uint) (*models.User, error)
-	UpdateFields(id uint, updates map[string]interface{}) error
+	FindByIdentifier(identifier string) (*models.User, error)
 
 	ExistsUsername(username string, excludeID uint) (bool, error)
 	ExistsEmail(email string, excludeID uint) (bool, error)
 	ExistsPhone(phone string, excludeID uint) (bool, error)
+
+	Create(user *models.User) error
+	Update(user *models.User) error
+	UpdateFields(id uint, updates map[string]interface{}) error
 }
--- internal/services/user/service.go
+++ internal/services/user/service.go
@@ -19,41 +19,7 @@ func NewUserService(repo repository.UserRepository) *UserService {
 func (s *UserService) GetByID(id uint) (*models.UserResponse, error) {
 	user, err := s.repo.FindByID(id)
 	if err != nil {
-		return nil, err
-	}
-	return NewUserResponse(user), err
-}
-
-func (s *UserService) Login(identifier, password string) (*models.UserResponse, error) {
-	user, err := s.repo.FindByIdentifier(identifier)
-	if err != nil {
-		return nil, errors.New("invalid credentials")
-	}
-
-	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
-	if err != nil {
-		return nil, errors.New("invalid credentials")
-	}
-
-	return NewUserResponse(user), nil
-}
-
-func (s *UserService) Create(user *models.User) (*models.UserResponse, error) {
-	err := s.repo.Create(user)
-	if err != nil {
-		return nil, errors.New("failed to create user")
-	}
-	return NewUserResponse(user), nil
-}
-
-func (s *UserService) Update(id uint, updates map[string]interface{}) (*models.UserResponse, error) {
-	err := s.repo.UpdateFields(id, updates)
-	if err != nil {
-		return nil, err
-	}
-	user, err := s.repo.FindByID(id)
-	if err != nil {
-		return nil, err
+		return nil, errors.New("user not found")
 	}
 	return NewUserResponse(user), err
 }
@@ -90,4 +56,38 @@ func (s *UserService) CheckUniqueness(excludeID uint, username, email, phone *st
 	}
 
 	return errorsMap, nil
+}
+
+func (s *UserService) Create(user *models.User) (*models.UserResponse, error) {
+	err := s.repo.Create(user)
+	if err != nil {
+		return nil, errors.New("failed to create user")
+	}
+	return NewUserResponse(user), nil
+}
+
+func (s *UserService) Update(id uint, updates map[string]interface{}) (*models.UserResponse, error) {
+	err := s.repo.UpdateFields(id, updates)
+	if err != nil {
+		return nil, err
+	}
+	user, err := s.repo.FindByID(id)
+	if err != nil {
+		return nil, err
+	}
+	return NewUserResponse(user), err
+}
+
+func (s *UserService) Login(identifier, password string) (*models.UserResponse, error) {
+	user, err := s.repo.FindByIdentifier(identifier)
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return nil, errors.New("invalid credentials")
+	}
+
+	return NewUserResponse(user), nil
 }
